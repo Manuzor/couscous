aa700b31b419312e67a78fe43255f25c24714bf5
Manuel Floruß <manuel.floruss@gmail.com>
2021-12-25 12:30:02 +0100

    update pull_subtrees script

--- lib/pull_subtrees.bat
+++ lib/pull_subtrees.bat
@@ -1,2 +1,3 @@
 @echo off
-git -C "%~dp0.." subtree pull --prefix lib/sokol-zig https://github.com/floooh/sokol-zig master --squash
+git -C "%~dp0.." subtree pull --prefix lib/sokol-zig https://github.com/floooh/sokol-zig master --squash %*
+git -C "%~dp0.." subtree pull --prefix lib/sokol-tools-bin https://github.com/floooh/sokol-tools-bin master --squash %*
