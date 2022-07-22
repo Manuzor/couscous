c63d75457a0b061328a9f7784e8df1c085d80485
Manuel Floruß <manuel.floruss@gmail.com>
2022-07-22 09:34:36 +0200

    Squashed 'lib/zig-clap/' changes from a2af4a9..996821a
    996821a Print multi value positionals correctly in `usage`
    ac5f465 Update help and usage examples
    984c229 New `help` api that provides options as to how paramters are printed.
    0970eb8 Fix long param parsing with '-' and '\_' in name
    b81787f Fix usage output in README
    5166a15 Add parseParams and friends
    651dc2c Refactor parseParam into a state machine
    dc70efb Workaround infinit loop caused by `try` inside `inline for`
    5f7b75d Allow for clap to parse argument values into types
    234af18 Bump actions/checkout from 2.4.0 to 3
    c06c936 Revert "Change clap into generating a struct"
    cfaac64 Change clap into generating a struct
    2d9da16 Fix minor typos in README.md
    
    git-subtree-dir: lib/zig-clap
    git-subtree-split: 996821a3e1f186c9e5cdfd971d742c9815ea590e

--- example/help.zig
+++ example/help.zig
@@ -2,17 +2,19 @@ const clap = @import("clap");
 const std = @import("std");
 
 pub fn main() !void {
-    const params = comptime [_]clap.Param(clap.Help){
-        clap.parseParam("-h, --help     Display this help and exit.         ") catch unreachable,
-        clap.parseParam("-v, --version  Output version information and exit.") catch unreachable,
-    };
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help     Display this help and exit.
+        \\-v, --version  Output version information and exit.
+        \\
+    );
 
-    var args = try clap.parse(clap.Help, &params, .{});
-    defer args.deinit();
+    var res = try clap.parse(clap.Help, &params, clap.parsers.default, .{});
+    defer res.deinit();
 
-    // clap.help is a function that can print a simple help message, given a
-    // slice of Param(Help). There is also a helpEx, which can print a
-    // help message for any Param, but it is more verbose to call.
-    if (args.flag("--help"))
-        return clap.help(std.io.getStdErr().writer(), &params);
+    // `clap.help` is a function that can print a simple help message. It can print any `Param`
+    // where `Id` has a `describtion` and `value` method (`Param(Help)` is one such parameter).
+    // The last argument contains options as to how `help` should print those parameters. Using
+    // `.{}` means the default options.
+    if (res.args.help)
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
 }
